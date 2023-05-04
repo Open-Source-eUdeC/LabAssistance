208e936c30248bcdf831ad2e462f41218d833607
CxrlosKenobi <kenobi@ieee.org>
2023-05-04 03:48:33 -0400

    Enhanced header UI/UX

--- src/Layout/components/Header/index.jsx
+++ src/Layout/components/Header/index.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from "react";
 import { motion, useScroll } from "framer-motion";
-import { IoShareOutline as ShareIcon } from 'react-icons/io5';
-import { FaBell as SubscribeIcon } from 'react-icons/fa';
+import { FaGoogleDrive as DriveIcon } from 'react-icons/fa';
+import { MdOutlineWhatsapp as WspIcon } from 'react-icons/md';
+import { useNavigate } from "react-router-dom";
 //
+import lab_data from "../../../data/lab.json";
 import "./index.scss";
 
 export default function Header({ profile_logo }) {
   const [scrolled, setScrolled] = useState(0);
   const { scrollYProgress } = useScroll();
+  const navigate = useNavigate();
 
   useEffect(() => {
     scrollYProgress.onChange((latest) => {
@@ -26,17 +29,35 @@ export default function Header({ profile_logo }) {
       <div className="header-container">
         <div className="spacer">
           <div className="header-icon">
-            <ShareIcon />
+            <a
+              href={lab_data.drive_repo}
+              target="_blank"
+              rel="noreferrer"
+              alt="Google Drive del lab"
+            >
+              <DriveIcon />
+            </a>
           </div>
         </div>
 
         <div className="spacer">
-          <img src={profile_logo} alt="profile logo" />
+          <img
+            alt="profile logo"
+            src={profile_logo}
+            onClick={() => navigate('/')}
+          />
         </div>
 
         <div className="spacer">
           <div className="header-icon">
-            <SubscribeIcon />
+            <a
+              href={lab_data.whatsapp_group}
+              target="_blank"
+              rel="noreferrer"
+              alt="Grupo de Whatsapp del lab"
+            >
+              <WspIcon />
+            </a>
           </div>
         </div>
       </div>
